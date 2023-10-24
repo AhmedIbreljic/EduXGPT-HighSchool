3a0afaa32a296bd3f20eb76bad893b177c067cf7
Ahmed Ibreljic <ahmedibreljic@gmail.com>
2023-10-24 11:10:48 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,7 @@
 import os
 
 os.environ['OPENAI_KEY'] = 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
+
+# Use api_key in your application
+
 # You can define other environment variables here as well
