7295a11c40a8b4ddc9a855e7cce07764631e31da
Ahmed Ibreljic <ahmedibreljic@gmail.com>
2023-10-24 14:08:56 -0400

    Update run.bat

--- src/run.bat
+++ src/run.bat
@@ -4,4 +4,6 @@ set CACHE_MODE=DISK
 set STORAGE_PATH=../data/storage
 set CACHE_PATH=../data/cache
 
+set OPENAI_API_KEY='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'
+
 streamlit run gui.py
